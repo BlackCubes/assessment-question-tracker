db9d6a1be5e464d3353eb2946cd3a7b1f85c085a
Elias Gutierrez <gutierrezelias1991@gmail.com>
2025-05-14 20:35:49 -0700

    feat(logging): add logger

---
+++ api/src/questiontracker/logging.py
@@ -0,0 +1,28 @@
+import logging
+
+from .config import LOG_LEVEL
+from .enums import QuestionTrackerEnum
+
+LOG_FMT_DBG = "%(levelname)s:%(message)s:%(pathname)s:%(funcName)s:%(lineno)d"
+
+
+class LogLevels(QuestionTrackerEnum):
+    INFO = "INFO"
+    WARN = "WARN"
+    ERROR = "ERROR"
+    DEBUG = "DEBUG"
+
+
+def configure_logging():
+    log_level = str(LOG_LEVEL).upper()
+    log_levels = list(LogLevels)
+
+    if log_level not in log_levels:
+        logging.basicConfig(level=LogLevels.ERROR)
+        return
+
+    if log_level == LogLevels.DEBUG:
+        logging.basicConfig(level=log_level, format=LOG_FMT_DBG)
+        return
+
+    logging.basicConfig(level=log_level)
