fa5e183831e06a29035c8994205196ae5f9bd747
Elias Gutierrez <gutierrezelias1991@gmail.com>
2025-05-17 14:05:09 -0700

    feat(users-models): add models

---
+++ backend/app/models/users.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+from typing import Optional
+from uuid import UUID
+
+from pydantic import EmailStr
+from sqlmodel import Field
+
+from .base import Base
+
+
+# Shared properties
+class UserBase(Base):
+    email: EmailStr = Field(unique=True, index=True, max_length=255)
+    full_name: str | None = Field(default=None, max_length=255)
+    is_active: bool = True
+    is_superuser: bool = False
+
+
+# Properties to receive via API on creation
+class UserCreate(UserBase):
+    password: str = Field(min_length=8, max_length=40)
+
+
+# Database model, database table inferred from class name
+class User(UserBase, table=True):
+    hashed_password: str
+
+
+# Properties to return via API
+class UserPublic(UserBase):
+    id: int
+    uuid: UUID
+    created_at: Optional[datetime]
+    updated_at: Optional[datetime]
