e70ae40a268289fd7d57ad994913ac1af884a120
Elias Gutierrez <gutierrezelias1991@gmail.com>
2025-05-17 13:17:36 -0700

    fix(config-core): change type from string to integer

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -34,21 +34,22 @@ class Settings(BaseSettings):
     FRONTEND_HOST: str = "http://localhost:3000"
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
 
-    BACKEND_CORS_ORIGINS: Annotated[list[AnyUrl] | str, BeforeValidator(parse_cors)] = (
-        []
-    )
+    BACKEND_CORS_ORIGINS: Annotated[
+        list[AnyUrl] | str,
+        BeforeValidator(parse_cors),
+    ] = []
 
     @computed_field
     @property
     def all_cors_origins(self) -> list[str]:
-        return [str(origin).rstrip("/") for origin in self.BACKEND_CORS_ORIGINS] + [
+        return [str(o).rstrip("/") for o in self.BACKEND_CORS_ORIGINS] + [
             self.FRONTEND_HOST
         ]
 
     PROJECT_NAME: str
 
     POSTGRES_SERVER: str
-    POSTGRES_PORT: str = 5432
+    POSTGRES_PORT: int = 5432
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str = ""
     POSTGRES_DB: str = ""
